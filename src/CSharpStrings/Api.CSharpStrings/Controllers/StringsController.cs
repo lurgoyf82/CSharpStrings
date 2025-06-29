6915953dd0b233e2265dfa9357fa80cd6919aee0
lurgoyf82 <lurgoyf82@hotmail.com>
2025-06-29 12:06:38 +0200

    Refactor solution structure and remove obsolete files
    Updated the solution to include new projects: `Api.CSharpStrings`, `Application.CSharpStrings`, `Domain.CSharpStrings`, and `Infrastructure.CSharpStrings`. Removed the old `CSharpStrings.csproj` file and restructured project dependencies accordingly.
    
    Modified the `CSharpStrings.http` file for updated host address and request format. Significant refactoring in `GetCartResponseHandler.cs` and removal of several DTO and entity classes, indicating a shift in functionality.
    
    Rewrote the `Program.cs` file to enhance application configuration and updated `launchSettings.json` for new development URLs.
    
    Additional changes include modifications to `CartOutputFormatter.cs`, `CartService.cs`, `InputParser.cs`, and `TaxCalculator.cs`, along with the removal of example data in `AddItemsToCartRequestDtoExample.cs`.
    
    Finally, logging configurations were added to `appsettings.Development.json` and `appsettings.json` to improve logging practices.

---
+++ src/CSharpStrings/Api.CSharpStrings/Controllers/StringsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.CSharpStrings.Controllers
+{
+    public class StringsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/CSharpStrings/Models/Entities/Cart.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CSharpStrings.Models.Entities
-{
-    public class Cart
-    {
-        public List<Item> Items { get; set; } = [];
-        public decimal Total { get; set; }
-        public decimal Tax { get; set; }
-    }
-}
